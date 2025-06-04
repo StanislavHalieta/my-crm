f73528c0b5521c79f65840698295a82bca0a9b52
HalietaSO <halieta.so@vmvpu.vn.ua>
2025-06-04 10:09:22 +0300

    backup

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useEffect, type FC } from "react";
 import { Grid } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../hooks/storeHooks";
@@ -164,6 +165,31 @@ const HomePage: FC = () => {
         </Grid> */}
       </StyledHomePageGrid>
     </StyledHomePage>
+=======
+import { Grid, Paper } from "@mui/material";
+import { FC } from "react";
+
+interface HomePageProps {}
+
+const HomePage: FC<HomePageProps> = () => {
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 5, sm: 4, md: 4 }}>
+          <Paper>size=8</Paper>
+        </Grid>
+        <Grid size={{ xs: 2, sm: 4, md: 4 }}>
+          <Paper>size=4</Paper>
+        </Grid>
+        <Grid size={{ xs: 2, sm: 4, md: 4 }}>
+          <Paper>size=4</Paper>
+        </Grid>
+        <Grid size={{ xs: 2, sm: 4, md: 4 }}>
+          <Paper>size=8</Paper>
+        </Grid>
+      </Grid>
+    </div>
+>>>>>>> 6c1c4fd (backup)
   );
 };
 
