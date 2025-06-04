6c1c4fd71734e41925a5cd4abc81c5fbd2add295
HalietaSO <halieta.so@vmvpu.vn.ua>
2025-06-04 10:09:22 +0300

    backup

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,27 @@
+import { Grid, Paper } from "@mui/material";
 import { FC } from "react";
 
-interface HomePageProps {
-    
-}
- 
+interface HomePageProps {}
+
 const HomePage: FC<HomePageProps> = () => {
-    return <div>Home</div>
-}
- 
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 5, sm: 4, md: 4 }}>
+          <Paper>size=8</Paper>
+        </Grid>
+        <Grid size={{ xs: 2, sm: 4, md: 4 }}>
+          <Paper>size=4</Paper>
+        </Grid>
+        <Grid size={{ xs: 2, sm: 4, md: 4 }}>
+          <Paper>size=4</Paper>
+        </Grid>
+        <Grid size={{ xs: 2, sm: 4, md: 4 }}>
+          <Paper>size=8</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
 export default HomePage;
