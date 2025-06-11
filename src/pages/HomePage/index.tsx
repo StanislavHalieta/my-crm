9a30de8a0381e1ea97232ca70eed338b6c53b897
Deimos <deimos1488@gmail.com>
2025-06-11 19:00:40 +0300

    Card without style

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,21 +3,42 @@ import { Card, CardContent, Typography, Grid, Box } from "@mui/material";
 import styled from "@emotion/styled";
 import { useAppDispatch, useAppSelector } from "../../hooks/storeHooks";
 import { fetchHomePage, selectHome } from "../../store/home/homeSlice";
-import { MainChart, MainPieChart } from "../../components";
+import { MainCard, MainChart, MainPieChart } from "../../components";
+import { ICard } from "../../components/MainCard";
+import { StyledHomePage, StyledHomePageGrid } from "./styles";
 
-const COLORS = ["#0057b7", "#ffd700", "#8884d8", "#82ca9d"];
-
-const Container = styled(Box)`
-  padding: 24px;
-  background: ${({ theme }) => theme.palette.background.default};
-  color: ${({ theme }) => theme.palette.primary.contrastText};
-`;
-
-const StyledCard = styled(Card)`
-  border-radius: 16px;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
-  background-color: ${({ theme }) => theme.palette.background.paper};
-`;
+const cards: ICard[] = [
+  {
+    description: "Вхідні: 140 | Вихідні: 172",
+    id: "1",
+    title: "Всього дзвінків: 312",
+  },
+  {
+    description: "Відправлено: 892 | Відкрито: 462",
+    id: "2",
+    title: "Email розсилки",
+  },
+  {
+    description: "Онлайн: 6 | Офлайн: 2",
+    id: "3",
+    title: "Активні оператори",
+  },
+  {
+    description: "Закриті: 140 | В роботі: 21",
+    id: "4",
+    title: "Активні задачі 21",
+  },
+  {
+    description: "ЕСВ: 81 504.92 грн | ПДВ: 25 354.54 грн",
+    id: "5",
+    title: "Податок",
+  },
+  {
+    description: "Онлайн: 6 | Офлайн: 2",
+    id: "6",
+    title: "Звітність",
+  },
+];
 
 const HomePage: FC = () => {
   const dispatch = useAppDispatch();
