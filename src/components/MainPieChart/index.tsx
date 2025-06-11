6ea1414439bd8c3286af3fc8ed4faca07fd97786
Deimos <deimos1488@gmail.com>
2025-06-11 16:26:19 +0300

    backup

--- src/components/MainPieChart/index.tsx
+++ src/components/MainPieChart/index.tsx
@@ -1,35 +1,30 @@
 import type { FC } from "react";
+import type { FC } from "react";
 import { StyledMainPieChart } from "./styles";
 // import { useTranslation } from "react-i18next";
 import { CardContent, Typography } from "@mui/material";
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 
-
-export interface IPieChart {
-  id: number | string;
-  value: number;
-  label: string;
-}
+// interface IPieChart {
+//   id: number | string;
+//   value: number;
+//   label: string;
+// }
 // const chartData = [
 //   { name: "Ліди", value: 400 },
 //   { name: "У роботі", value: 300 },
 //   { name: "Угода", value: 200 },
 //   { name: "Закрито", value: 100 },
 // ];
-interface MainPieChartProps {
-  data:IPieChart[] 
-}
-const chartData:IPieChart[] = [
+const chartData = [
   { id: 0, value: 5, label: "charts.unpaid" },
   { id: 1, value: 35, label: "charts.paid" },
 ];
 
 const COLORS = ["#0057b7", "#ffd700", "#8884d8", "#82ca9d"];
 
-const COLORS = ["#0057b7", "#ffd700", "#8884d8", "#82ca9d"];
-
-const MainPieChart: FC<MainPieChartProps> = ({data}) => {
+const MainPieChart: FC<MainPieChartProps> = () => {
   // const { t } = useTranslation();
   return (
     <StyledMainPieChart>
@@ -40,7 +35,7 @@ const MainPieChart: FC<MainPieChartProps> = ({data}) => {
         <ResponsiveContainer width="100%" height={250}>
           <PieChart>
             <Pie data={chartData} dataKey="value" outerRadius={80} label>
-              {data.map((_, index) => (
+              {chartData.map((_, index) => (
                 <Cell
                   key={`cell-${index}`}
                   fill={COLORS[index % COLORS.length]}
