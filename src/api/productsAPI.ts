69a30431bff530d609b9a7eff867ae517dd40808
Stanislav <halieta.so@vmvpu.vn.ua>
2025-06-11 14:17:47 +0300

    HTTPService add

--- src/api/productsAPI.ts
+++ src/api/productsAPI.ts
@@ -1,19 +1,17 @@
 import HttpService from "../services/HTTPService";
-import { IProduct, IProductsResp } from "../store/home/types";
+import { IProduct } from "../store/home/types";
 
 const api = new HttpService("https://dummyjson.com", {
   "Content-Type": "application/json",
 });
 
-// Auth token
-// const jwtToken = "";
-// api.setAuthToken(jwtToken);
+const jwtToken = "";
+
+api.setAuthToken(jwtToken);
 
 export async function fetchProductsAPI() {
   try {
-    const resp = await api.get<IProductsResp>("/products");
-
-    return resp.data
+    return await api.get<IProduct[]>("/products");
   } catch (error) {
     return error;
   }
@@ -21,7 +19,7 @@ export async function fetchProductsAPI() {
 
 export async function fetchProductAPI(productID: string | number) {
   try {
-    return await api.get<IProduct>(`/products/${productID}`);
+    return await api.get<IProduct[]>(`/products/${productID}`);
   } catch (error) {
     return error;
   }
